9a4ac47ec7441ca521b627b52f2560b17c8faf6d
Vallejos3 <controlplane.sys@gmail.com>
2026-04-24 03:31:57 +0000

    add plausible analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col"><Script strategy="afterInteractive" src="https://plausible.io/js/script.js" data-domain="privatepdf.pages.dev" />{children}</body>
     </html>
   );
 }
